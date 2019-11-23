8e4ee743ce46c4b0f8bb97c0453790bb39cdf574
Joshua Yuen <joshuayuen99@gmail.com>
2019-11-22 23:46:29 -0500

    Added "emoji" command

---
+++ commands/utility/emoji.js
@@ -0,0 +1,31 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+    name: "emoji",
+    aliases: ["unicode", "uni"],
+    category: "utility",
+    description: "Gives the unicode for the entered emoji(s).",
+    usage: "emoji <emoji> [...emoji] ",
+    run: async (client, message, args) => {
+        if(!args[0]) return message.reply("Please enter an emoji");
+
+        let emojisList;
+        for(const word of args) {
+            if(!emojisList) {
+                emojisList = `\`\\u${toUni(word)}\``;
+                continue;
+            }
+            emojisList += `, \`\\u${toUni(word)}\``;
+        }
+
+        message.channel.send(stripIndents`${args.join(" ")} Emoji/Character info:
+        ${emojisList}`);
+    }
+};
+
+const toUni = function (str) {
+    if (str.length < 4)
+      return str.codePointAt(0).toString(16);
+    return str.codePointAt(0).toString(16) + '-' + str.codePointAt(2).toString(16);
+  };
