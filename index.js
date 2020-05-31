9b2e605ea1fb5d43f4006e448bb89d30afb7a50a
Joshua Yuen <joshuayuen99@gmail.com>
2020-05-31 19:07:24 -0400

    Added logging for when the bot leaves a server

--- index.js
+++ index.js
@@ -108,17 +108,45 @@ function setup() {
 		const owner = await client.users.fetch(process.env.OWNERID);
 
 		const embedMsg = new MessageEmbed()
-				.setColor("#0d6adb")
-				.setTimestamp(guild.joinedTimestamp)
+				.setColor("GREEN")
+				.setTimestamp()
 				.setFooter(guild.name, guild.iconURL())
-				.setAuthor("Joined server", guild.iconURL())
+				.setAuthor("Joined server :)", guild.iconURL())
 				.addField("Guild information", stripIndents`**\\> ID:** ${guild.id}
 				**\\> Name:** ${guild.name}
 				**\\> Member count:** ${guild.memberCount}
-				**\\> Joined at:** ${formatDate(guild.joinedTimestamp)}`, true);
+				**\\> Created at:** ${formatDateLong(guild.createdTimestamp)}
+				**\\> Joined at:** ${formatDateLong(guild.joinedTimestamp)}`)
+				.addField("Server owner information", stripIndents`**\\> ID:** ${guild.owner.user.id}
+				**\\> Username:** ${guild.owner.user.username}
+				**\\> Discord Tag:** ${guild.owner.user.tag}
+				**\\> Created account:** ${formatDate(guild.owner.user.createdAt)}`, true);
 
 		owner.send(embedMsg);
 	});
+
+	client.on("guildDelete", async guild => {
+		console.log(`Left guild: ${guild.name}`);
+
+		const owner = await client.users.fetch(process.env.OWNERID);
+
+		const embedMsg = new MessageEmbed()
+				.setColor("RED")
+				.setTimestamp()
+				.setFooter(guild.name, guild.iconURL())
+				.setAuthor("Left server :(", guild.iconURL())
+				.addField("Guild information", stripIndents`**\\> ID:** ${guild.id}
+				**\\> Name:** ${guild.name}
+				**\\> Member count:** ${guild.memberCount}
+				**\\> Created at:** ${formatDateLong(guild.createdTimestamp)}
+				**\\> Joined at:** ${formatDateLong(guild.joinedTimestamp)}`)
+				.addField("Server owner information", stripIndents`**\\> ID:** ${guild.owner.user.id}
+				**\\> Username:** ${guild.owner.user.username}
+				**\\> Discord Tag:** ${guild.owner.user.tag}
+				**\\> Created account:** ${formatDate(guild.owner.user.createdAt)}`, true);
+
+		owner.send(embedMsg);
+	})
 
 	client.on("message", async message => {
 		if (message.author.bot) return;  // if a bot sent the message
