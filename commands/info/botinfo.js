af2607674c3c7abb2d470ae7cceffa7b15c64994
Joshua Yuen <joshuayuen99@gmail.com>
2020-09-22 05:46:53 -0400

    Added invite link to botinfo command

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -15,8 +15,9 @@ module.exports = {
             .setColor("#0b7ed6")
             .setThumbnail(bicon)
             .addField("Bot name", client.user.username)
-			.addField("My owner", `${process.env.OWNERNAME}${process.env.OWNERTAG}`)
-			.addField("Source code", "https://github.com/joshuayuen99/discordbot")
+            .addField("My owner", `${process.env.OWNERNAME}${process.env.OWNERTAG}`)
+            .addField("Invite link", "[Invite me to your server!](https://discord.com/api/oauth2/authorize?client_id=647196546492006423&permissions=309587062&scope=bot)")
+            .addField("Source code", "https://github.com/joshuayuen99/discordbot")
 			.addField("Server count", client.guilds.cache.size)
             .addField("Created at", formatDateLong(client.user.createdAt));
 
