34f39e994195a1884cee66fcf7d7ffa71685f885
Joshua Yuen <joshuayuen99@gmail.com>
2020-09-23 17:59:10 -0400

    await eval command

--- commands/ownerCommands/eval.js
+++ commands/ownerCommands/eval.js
@@ -8,7 +8,7 @@ module.exports = {
 
         try {
             const code = args.join(" ");
-            let evaled = eval(code);
+            let evaled = await eval(code);
 
             if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 
