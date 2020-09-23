cdbc9229a77c87387bf10e7f7053f807cd2083ea
Joshua Yuen <joshuayuen99@gmail.com>
2020-09-22 20:17:03 -0400

    Added coin command

---
+++ commands/utility/coin.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "coin",
+    aliases: ["flip"],
+    category: "utility",
+    description: "Flips a coin!",
+    usage: "coin",
+    run: async (client, message, args) => {
+        const result = Math.round(Math.random());
+
+        let resultString;
+        if (result == 0) resultString = "Heads!";
+        else resultString = "Tails!";
+
+        const embedMsg = new MessageEmbed()
+            .setColor(client.user.displayHexColor)
+            .setFooter(message.guild.me.displayName, client.user.displayAvatarURL())
+            .setTimestamp()
+            .setDescription(`${message.author} Flipped a coin!`)
+            .addField(`Result`, resultString);
+
+        return message.channel.send(embedMsg);
+    }
+};
--- commands/utility/rng.js
+++ commands/utility/rng.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-const { stripIndents } = require("common-tags");
 
 const DICE = "🎲";
 
