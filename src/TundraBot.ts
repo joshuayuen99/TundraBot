b768a08cc88dc433e21c5c654a7142cd866f47df
Joshua Yuen <joshuayuen99@gmail.com>
2021-07-19 18:55:48 -0400

    Added try catch for fatal errors

--- src/TundraBot.ts
+++ src/TundraBot.ts
@@ -8,23 +8,33 @@ import fetch from "node-fetch";
 import { exit } from "process";
 
 function setup() {
-    const result = dotenv.config({
-        path: __dirname + "/../.env",
-    });
-    if (result.error) {
-        Logger.log("error", `Error loading .env file:\n${result.error.message}`);
+    try {
+        const result = dotenv.config({
+            path: __dirname + "/../.env",
+        });
+        if (result.error) {
+            Logger.log("error", `Error loading .env file:\n${result.error.message}`);
+            exit(1);
+        }
+    
+        const client = new TundraBot();
+    
+        // Login
+        client.login(process.env.DISCORDTOKEN).catch((err) => {
+            Logger.log("error", err);
+        });
+        
+        try {
+            // Start dashboard server
+            new Server(client);
+        } catch (err) {
+            Logger.log("error", `Trouble starting dashboard server:\n${err}`);
+        }
+    } catch (err) {
+        Logger.log("error", `Fatal error:\n${err}`);
         exit(1);
     }
-
-    const client = new TundraBot();
-
-    // Login
-    client.login(process.env.DISCORDTOKEN).catch((err) => {
-        Logger.log("error", err);
-    });
-
-    // Start dashboard server
-    new Server(client);
+    
 }
 
 // if there is an unhandledRejection, log them
