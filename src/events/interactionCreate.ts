74ae3ac6cfc226af4dcf916f08e5e77b292f9ee8
Joshua Yuen <joshuayuen99@gmail.com>
2021-08-28 15:31:14 -0400

    Improve slash command error handling

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -22,86 +22,95 @@ export default class InteractionCreateHandler extends EventHandler {
     async invoke(interaction: Interaction): Promise<void> {
         if (!interaction.isCommand()) return;
 
-        const interactionCommand = this.client.interactiveCommands.get(
-            interaction.commandName
-        );
-
-        if (interactionCommand) {
-            let guildSettings: Partial<guildInterface>;
-            if (interaction.inGuild()) {
-                guildSettings = await this.DBGuildManager.get(
-                    interaction.guild
-                );
-            } else {
-                guildSettings = {};
-            }
-
-            const ctx = new SlashCommandContext(
-                this.client,
-                interactionCommand,
-                guildSettings,
-                interaction
+        try {
+            const interactionCommand = this.client.interactiveCommands.get(
+                interaction.commandName
             );
 
-            let userCooldown = this.cmdCooldown[interaction.user.id];
-            if (!userCooldown) {
-                // user hasn't run a command yet
-                this.cmdCooldown[interaction.user.id] = {};
-                userCooldown = this.cmdCooldown[interaction.user.id];
-            }
-
-            const time = userCooldown[interaction.commandName] || 0;
-            if (
-                time &&
-                time > Date.now() &&
-                interaction.user.id !== process.env.OWNERID
-            ) {
-                const seconds = Math.ceil((time - Date.now()) / 1000);
-
-                const cooldownEmbed = new MessageEmbed()
-                    .setColor("RED")
-                    .setDescription(
-                        `❌ You must wait \`${seconds} second${
-                            seconds > 1 ? "s" : ""
-                        }\` to use \`${interaction.commandName}\` again.`
+            if (interactionCommand) {
+                let guildSettings: Partial<guildInterface>;
+                if (interaction.inGuild()) {
+                    guildSettings = await this.DBGuildManager.get(
+                        interaction.guild
                     );
+                } else {
+                    guildSettings = {};
+                }
 
-                ctx.commandInteraction.reply({
-                    embeds: [cooldownEmbed],
-                    ephemeral: true,
-                });
-                return;
-            }
-            this.cmdCooldown[interaction.user.id][interaction.commandName] =
-                Date.now() + interactionCommand.cooldown;
+                const ctx = new SlashCommandContext(
+                    this.client,
+                    interactionCommand,
+                    guildSettings,
+                    interaction
+                );
 
-            if (interaction.inGuild()) {
-                // guild
-                if (!validateBotPermissions(ctx)) return;
-                if (!validateMemberPermissions(ctx)) return;
-            }
+                let userCooldown = this.cmdCooldown[interaction.user.id];
+                if (!userCooldown) {
+                    // user hasn't run a command yet
+                    this.cmdCooldown[interaction.user.id] = {};
+                    userCooldown = this.cmdCooldown[interaction.user.id];
+                }
 
-            // have to do different check because "strict" is disabled in tsconfig
-            if (!interaction.guild) {
-                // DM
-                if (interactionCommand.guildOnly) {
-                    interaction;
-                    const guildOnlyEmbed = new MessageEmbed()
+                const time = userCooldown[interaction.commandName] || 0;
+                if (
+                    time &&
+                    time > Date.now() &&
+                    interaction.user.id !== process.env.OWNERID
+                ) {
+                    const seconds = Math.ceil((time - Date.now()) / 1000);
+
+                    const cooldownEmbed = new MessageEmbed()
                         .setColor("RED")
                         .setDescription(
-                            `❌ \`${interaction.commandName}\` is not useable in DM's.`
+                            `❌ You must wait \`${seconds} second${
+                                seconds > 1 ? "s" : ""
+                            }\` to use \`${interaction.commandName}\` again.`
                         );
 
                     ctx.commandInteraction.reply({
-                        embeds: [guildOnlyEmbed],
+                        embeds: [cooldownEmbed],
+                        ephemeral: true,
                     });
                     return;
                 }
+                this.cmdCooldown[interaction.user.id][interaction.commandName] =
+                    Date.now() + interactionCommand.cooldown;
+
+                if (interaction.inGuild()) {
+                    // guild
+                    if (!validateBotPermissions(ctx)) return;
+                    if (!validateMemberPermissions(ctx)) return;
+                }
+
+                // have to do different check because "strict" is disabled in tsconfig
+                if (!interaction.guild) {
+                    // DM
+                    if (interactionCommand.guildOnly) {
+                        interaction;
+                        const guildOnlyEmbed = new MessageEmbed()
+                            .setColor("RED")
+                            .setDescription(
+                                `❌ \`${interaction.commandName}\` is not useable in DM's.`
+                            );
+
+                        ctx.commandInteraction.reply({
+                            embeds: [guildOnlyEmbed],
+                        });
+                        return;
+                    }
+                }
+
+                Logger.log("cmd", `/${ctx.commandInteraction.commandName}`);
+
+                interactionCommand.slashCommandExecute(ctx).catch((err) => {
+                    Logger.log(
+                        "error",
+                        `Error running slash command </${ctx.commandInteraction.commandName}>:\n${err}`
+                    );
+                });
             }
-
-            Logger.log("cmd", `/${ctx.commandInteraction.commandName}`);
-
-            await interactionCommand.slashCommandExecute(ctx);
+        } catch (err) {
+            Logger.log("error", `interactionCreate event error:\n${err}`);
         }
     }
 }
