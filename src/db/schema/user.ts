3cd640a7ae97af12c921735134f503eb0d569b11
Joshua Yuen <joshuayuen99@gmail.com>
2026-03-28 14:24:51 -0400

    refactor: Rename userTable to usersTable

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -2,13 +2,13 @@ import { pgTable, bigint } from "drizzle-orm/pg-core";
 import { timestamps } from "../columnHelpers.ts";
 import { DB } from "../db.ts";
 
-export const userTable = pgTable("users", {
+export const usersTable = pgTable("users", {
     id: bigint({ mode: "bigint" }).primaryKey(),
     ...timestamps,
 });
 
-export type User = typeof userTable.$inferInsert;
+export type User = typeof usersTable.$inferInsert;
 
 export async function insertUser(user: User) {
-    return DB.insert(userTable).values(user);
+    return DB.insert(usersTable).values(user);
 }
