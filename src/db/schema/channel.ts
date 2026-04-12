d692074539ae5e99e9dc12e28903137d32c5d40d
Joshua Yuen <joshuayuen99@gmail.com>
2026-04-11 22:32:34 -0400

    fix(channel schema): Correctly reference the guilds table as a foreign key

--- src/db/schema/channel.ts
+++ src/db/schema/channel.ts
@@ -1,10 +1,11 @@
 import { pgTable, bigint } from "drizzle-orm/pg-core";
 
 import { timestamps } from "../columnHelpers.ts";
+import { guildsTable } from "./guild.ts";
 
 export const channelsTable = pgTable("channels", {
     id: bigint({ mode: "bigint" }).primaryKey(),
-    guildId: bigint({ mode: "bigint" }),
+    guildId: bigint({ mode: "bigint" }).references(() => guildsTable.id),
     ...timestamps,
 });
 
